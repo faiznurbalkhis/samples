22b9f8b0893d9335a79c9093179702a75d91dde0
faiznurbalkhis <faiznurbalkhis96@gmail.com>
2021-03-18 20:04:34 +0800

    New location commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,19 @@
+package samples.demo;
+
+public class Location {
+	
+	private int height;
+	
+	public void print() {
+		System.out.println("height");
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+}
